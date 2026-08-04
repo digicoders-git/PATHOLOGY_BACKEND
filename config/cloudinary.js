f74463be66eb07738de6122a5d52f769cdaead19
SHIVA VISHWAKARMA <raw66010@gmail.com>
2026-08-04 19:13:26 +0530

    add fallback cloudinary configuration

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -3,9 +3,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'dazumf2fc',
+  api_key: process.env.CLOUDINARY_API_KEY || '936979368812817',
+  api_secret: process.env.CLOUDINARY_API_SECRET || '2Q81L1UbrQelnxUgeG_QTGxgSn8',
 });
 
 export default cloudinary.v2;
